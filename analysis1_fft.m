585b6bd97f9bb132fe30d220f83d7669b1888cbd
Zoe Kendall <zskendall@gmail.com>
2016-06-21 16:55:25 -0500

    add butterworth

--- analysis1_fft.m
+++ analysis1_fft.m
@@ -12,16 +12,19 @@ home;
 filename = 'C:\Users\zkendall\zkendall\Documents\SPIRE-EIT\srest1.xlsx';
 desired_electrode = 'Cz';
 
-sampleRate = 2048;
+sampleRate = 2048;      % Hz
 duration = 10;
 N = sampleRate*duration;
 
 % ========================================================================
 %                                SCRIPT
 % ========================================================================
-% read data and get the electrode labels as a cell array
 [num, txt, raw] = xlsread(filename);
+
+read data and get the electrode labels as a cell array
 labels = raw(9, 1:end);
+frames = num(7:end, 1);
+time = frames ./ sampleRate;
 
 % find the column number of the desired electrode
 elec = 1;
@@ -31,9 +34,46 @@ for i = 1:length(labels)
         break;
     end
 end
+% elec
+% labels(elec)
+data = num(7:end,1:end);
+doi = data(1:end, elec);
+
+% Hopefully, the next two lines bandpass filters our data.
+bpFilt = designfilt('bandpassFIR', 'FilterOrder', 20, 'CutoffFrequency1', 1, 'CutoffFrequency2', 65, 'SampleRate', sampleRate);
+dataOut = filter(bpFilt,doi);
+
+d = fdesign.notch(2, 60, 35, 2048);
+Hd = design(d);
+newData = filter(Hd, dataOut);
+
+[b,a] = butter(2, 1*2/sampleRate);
+butterData1 = filter(b,a,dataOut);
+[x,y] = butter(2, 62.5*2/sampleRate);
+butterData2 = filter(x,y,dataOut);
+
+% [num,txt,raw] = xlsread(filename, 'F9:F9')
+% if strcmp(txt, 'F1') == 1
+%     raw = xlsread(filename, 'F:F');
+%     x = 1
+% end
+
+% X = fft(newData);
 
 % plot the data
-data = num(7:end,1:end);
-plot(data(1:end,elec))
-xlabel('Sample')
+figure(1)
+plot(time, doi)
+title('Original Signal')
+xlabel('Time (s)')
 ylabel('EEG (V)')
+
+figure(2)
+subplot(2,1,1), plot(time, dataOut)
+subplot(2,1,2), plot(time, newData)
+xlabel('Time (s)')
+ylabel('EEG (V)')
+%ylim([1e-3, 1.6e-3])
+
+figure(3)
+plot(time, butterData2)
+ylim([1e-3,1.6e-3])
