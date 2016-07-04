40fb91113049ae0e40e36d3ce827f2a4fbe4e396
Zoe Kendall <zskendall@gmail.com>
2016-07-04 18:34:22 -0500

    we're desperate :(

--- analysis1_fft.m
+++ analysis1_fft.m
@@ -19,12 +19,12 @@ N = sampleRate*duration;
 % ========================================================================
 %                                SCRIPT
 % ========================================================================
-%[num, txt, raw] = xlsread(filename);
+[num, txt, raw] = xlsread(filename);
 
 % read data and get the electrode labels as a cell array
-%labels = raw(9, 1:end);
-%frames = num(7:end, 1);
-%time = frames ./ sampleRate;
+labels = raw(9, 1:end);
+frames = num(7:end, 1);
+time = frames ./ sampleRate;
 
 % find the column number of the desired electrode
 elec = 1;
@@ -36,27 +36,33 @@ for i = 1:length(labels)
 end
 % elec
 % labels(elec)
-%data = num(7:end,1:end);
-%doi = data(1:end, elec);
+data = num(7:end,1:end);
+doi = data(1:end, elec);
+
+% Hopefully, this will filter out the electricity.
+d = fdesign.notch(2, 60, 35, 2048);
+Hd = design(d);
+dataOut = filter(Hd, doi);
 
 % Hopefully, the next two lines bandpass filters our data.
 bpFilt = designfilt('bandpassFIR', 'FilterOrder', 20, 'CutoffFrequency1', 1, 'CutoffFrequency2', 65, 'SampleRate', sampleRate);
-dataOut = filter(bpFilt,doi);
-
-d = fdesign.notch(2, 60, 35, 2048);
-Hd = design(d);
-newData = filter(Hd, dataOut);
+newData = filter(bpFilt,dataOut);
 
 [b,a] = butter(2, 1*2/sampleRate);
 butterData1 = filter(b,a,dataOut);
 [x,y] = butter(2, 62.5*2/sampleRate);
 butterData2 = filter(x,y,butterData1);
 
+% lapl = fspecial(laplacian) //makes the kernel/filter
+% finalData = filter2(lapl, butterData2) //apply the filter you just made
+
 % [num,txt,raw] = xlsread(filename, 'F9:F9')
 % if strcmp(txt, 'F1') == 1
 %     raw = xlsread(filename, 'F:F');
 %     x = 1
 % end
+
+% laplac = eeg_laplac(butterData2, 1);
 
 % X = fft(butterData2);
 
@@ -68,11 +74,12 @@ xlabel('Time (s)')
 ylabel('EEG (V)')
 
 figure(2)
-subplot(2,1,1), plot(time, dataOut)
-subplot(2,1,2), plot(time, newData)
+%subplot(2,1,1), plot(time, dataOut)
+%subplot(2,1,2), plot(time, newData)
+plot(time, newData)
 xlabel('Time (s)')
 ylabel('EEG (V)')
-%ylim([1e-3, 1.6e-3])
+ylim([1e-3, 1.6e-3])
 
 figure(3)
 plot(time, butterData2)
